6f969b72bedd08b303613655adfc3275c2583353
Wouter Louwman <wouterlouwman52@gmail.com>
2025-10-28 15:06:40 +0100

    feat/unique_words changed the code to see if will work

--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -2,5 +2,5 @@ import textutils.core as c
 def test_unique_words_pipeline():
     text = "Red red BLUE"
     final_test=text.lower()
-    result = c.test_unique_words(text)
-    assert result == ["red", "blue"]
+    result = c.unique_words(text)
+    assert result == ["blue", "red"]
