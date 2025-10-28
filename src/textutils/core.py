3ef01f4d65e4ea7b5a7d1e889fab2f42d77b7a52
Wouter Louwman <wouterlouwman52@gmail.com>
2025-10-28 14:34:59 +0100

    feat/unique_words wrong name again changed it back to test

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,5 +1,5 @@
 import textutils.core as c
 
-def unique_words():
+def test_unique_words():
     text = "Red red BLUE"
     assert c.unique_words(text) == ["Red", "red", "BLUE"]
--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -1,5 +1,5 @@
 import textutils.core as c
 def unique_words_pipeline():
     text = "Red red BLUE"
-    result = c.unique_words(text)
+    result = c.test_unique_words(text)
     assert result == ["Red", "red", "BLUE"]
