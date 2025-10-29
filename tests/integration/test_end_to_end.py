d4de96a950ac7607a2ab802a4672f7000736e57a
Botond <botond.fazekas99@gmail.com>
2025-10-29 11:57:10 +0100

    sentence-count put it into test:end

---
+++ tests/integration/test_end_to_end.py
@@ -0,0 +1,17 @@
+mport textutils.core as c
+
+def test_count_sentences_basic():
+    text = "Hello world. How are you? Bye!"
+    assert c.count_sentences(text) == 3
+
+def test_count_sentences_trailing_spaces():
+    text = "Test.  "
+    assert c.count_sentences(text) == 1
+
+def test_count_sentences_empty():
+    text = ""
+    assert c.count_sentences(text) == 0
+
+def test_count_sentences_mixed_punctuation():
+    text = "A! B? C."
+    assert c.count_sentences(text) == 3
